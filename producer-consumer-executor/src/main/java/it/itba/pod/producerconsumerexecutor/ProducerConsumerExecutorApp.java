add5509b0372e0d91e991c8cde1b9a50cfedf469
Santiago Ramirez Ayuso <santiago.ramirez@mulesoft.com>
2014-08-25 01:36:38 -0300

    Add consumer producer implementation using executor

--- producer-consumer-executor/src/main/java/it/itba/pod/producerconsumerexecutor/ProducerConsumerExecutorApp.java
+++ producer-consumer-executor/src/main/java/it/itba/pod/producerconsumerexecutor/ProducerConsumerExecutorApp.java
@@ -5,18 +5,25 @@
 * MuleSoft. If such an agreement is not in place, you may not use the software.
 */
 
-package it.itba.pod.producerconsumer;
+package it.itba.pod.producerconsumerexecutor;
+
+import it.itba.pod.producerconsumer.Consumer;
+import it.itba.pod.producerconsumer.Producer;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-public class ProducerConsumerApp
+public class ProducerConsumerExecutorApp
 {
     private static int MAX_QUEUE_SIZE = 100;
 
     private static int PRODUCER_THREAD_NUM = 4;
 
     private static int CONSUMER_THREAD_NUM = 2;
+
+    private static final Executor executor = Executors.newFixedThreadPool(PRODUCER_THREAD_NUM + CONSUMER_THREAD_NUM);
 
     public static void main(String[] args)
     {
@@ -25,15 +32,13 @@ public class ProducerConsumerApp
         for (int i = 0; i < PRODUCER_THREAD_NUM; i++)
         {
             Producer producer =  new Producer(queue, MAX_QUEUE_SIZE);
-            Thread producerThread = new Thread(producer);
-            producerThread.start();
+            executor.execute(producer);
         }
 
         for (int i = 0; i < CONSUMER_THREAD_NUM; i++)
         {
             Consumer consumer =  new Consumer(queue);
-            Thread consumerThread = new Thread(consumer);
-            consumerThread.start();
+            executor.execute(consumer);
         }
     }
 }
